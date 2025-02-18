b258afd37da9628d077d3a05e3c0e870e13e67ee
Jose <josesitocrack0429p@gmail.com>
2025-02-18 18:54:35 -0500

    🥳 Projecto con flask

--- main.py
+++ main.py
@@ -9,11 +9,21 @@ facts_list = ["Elon Musk afirma que las redes sociales están diseñadas para ma
             "Las redes sociales tienen aspectos positivos y negativos, y debemos ser conscientes de ambos cuando utilicemos estas plataformas.", 
             "El estudio de la adicción tecnológica es una de las áreas más relevantes de la investigación científica moderna."]
 
+# Funcion generador contraseña
+password_caract = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+"
+def generar_password(length=12):
+    password = ""
+    for i in range(length):
+        password += random.choice(password_caract)
+    return password
+
+
 @app.route("/")
 def hello_world():
     return '''<h1>Web en flask por Joss</h1>
     <p>Esto es un parrafo</p>
     <a href="/random_fact">Random Fact</a>
+    <a href="/password_generator">Genera contraseña aleatoria</a>
     '''
 
 @app.route("/random_fact")
@@ -23,5 +33,15 @@ def random_fact():
     <button><a href='/random_fact'>Ver otro dato</a></button>
     <a href="/">Regresar</a>
     '''
+
+# Generador de contraseñas    
+@app.route("/password_generator")
+def password_generator():
+    password = generar_password()
+    return f'''<h3>Generador de contraseñas</h3>
+                <p>La contraseña que generaste es: {password}</p>
+                <a href="/">Regresar</a>
+                '''
+
 
 app.run(debug=True)
